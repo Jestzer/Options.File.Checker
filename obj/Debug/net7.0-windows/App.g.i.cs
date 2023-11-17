b48ba3c27a1753d35810fa18b67fb9a19ba4b2e8
Jestzer <james@buonopane.email>
2023-11-17 15:14:07 -0500

    Quick fixes.

--- obj/Debug/net7.0-windows/App.g.i.cs
+++ obj/Debug/net7.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Options.File.Checker.WPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Options.File.Checker.WPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public static void Main() {
             Options.File.Checker.WPF.App app = new Options.File.Checker.WPF.App();
             app.InitializeComponent();
