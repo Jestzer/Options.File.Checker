8fadb642812389be2a487464f7f25747138fe7b2
Jestzer <james@buonopane.email>
2023-11-30 09:39:05 -0500

    Removes whitespaces before INCLUDE, EXCLUDE, & RESERVE.

--- obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -133,7 +133,7 @@ namespace Options.File.Checker.WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Options.File.Checker.WPF;V0.1.1.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Options.File.Checker.WPF;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
