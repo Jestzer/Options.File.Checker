3e5ef12a8899b33bdd32504e7055bcdc92dbd8cb
Jestzer <james@buonopane.email>
2023-11-30 13:46:02 -0500

    Prototyping a new error dialog to make things less confusing.

---
+++ ErrorWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+
+namespace Options.File.Checker.WPF
+{
+    public partial class ErrorWindow : Window
+    {
+        public ErrorWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Implement window dragging with the mouse.
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
