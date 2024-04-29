5208b01504b56232c36a33fc7eeaabeca48b3736
Jestzer <126624248+Jestzer@users.noreply.github.com>
2024-04-29 17:56:00 -0400

    Beginning work on analyzer class.

---
+++ LicenseAndOptionsFileDataAnalyzer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Options.File.Checker.WPF
+{
+    public partial class LicenseAndOptionsFileDataAnalyzer
+    {
+        public static (
+            bool serverLineHasPort,
+            bool daemonLineHasPort,
+            bool caseSensitivity,
+            Dictionary<int, Tuple<string, int, string, string, string>>? licenseFileDictionary,
+            Dictionary<int, Tuple<string, string, string, string, string>>? includeDictionary,
+            Dictionary<int, Tuple<string, string, string, string, string>>? includeBorrowDictionary,
+            Dictionary<int, Tuple<string, string>>? includeAllDictionary,
+            Dictionary<int, Tuple<string, string, string, string, string>>? excludeDictionary,
+            Dictionary<int, Tuple<string, string, string, string, string>>? excludeBorrowDictionary,
+            Dictionary<int, Tuple<string, string>>? excludeAllDictionary,
+            Dictionary<int, Tuple<int, string, string, string, string, string>>? reserveDictionary,
+            Dictionary<string, Tuple<int, string, string>>? maxDictionary,
+            Dictionary<int, Tuple<string, string, int>>? groupDictionary,
+            Dictionary<int, Tuple<string, string>>? hostGroupDictionary,
+            string? err)
+
+            AnalyzeData(string licenseFilePath, string optionsFilePath)
+        {
+            var (serverLineHasPort,
+                daemonLineHasPort,
+                caseSensitivity,
+                licenseFileDictionary,
+                includeDictionary,
+                includeBorrowDictionary,
+                includeAllDictionary,
+                excludeDictionary,
+                excludeBorrowDictionary,
+                excludeAllDictionary,
+                reserveDictionary,
+                maxDictionary,
+                groupDictionary,
+                hostGroupDictionary,
+                err) = LicenseAndOptionsFileDataGatherer.GatherData(licenseFilePath, optionsFilePath);
+            var analysisResult = LicenseAndOptionsFileDataGatherer.GatherData(licenseFilePath, optionsFilePath);
+            
+            foreach (var includeEntry in includeDictionary)
+            {
+                
+            }
+            foreach (var excludeEntry in excludeDictionary)
+            {
+
+            }
+            return (serverLineHasPort, daemonLineHasPort, caseSensitivity, licenseFileDictionary, includeDictionary, includeBorrowDictionary, includeAllDictionary, excludeDictionary, excludeBorrowDictionary, excludeAllDictionary, reserveDictionary, maxDictionary, groupDictionary, hostGroupDictionary, null);
+        }
+    }
+}
--- LicenseAndOptionsFileDataGatherer.cs
+++ LicenseAndOptionsFileDataGatherer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Windows.Shapes;
 
 namespace Options.File.Checker.WPF
 {
