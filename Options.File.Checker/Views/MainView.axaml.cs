57942bbe1b6d943bb9ca7a818b3106962c005259
Jestzer <126624248+Jestzer@users.noreply.github.com>
2024-05-22 15:47:57 -0400

    It may seem like I've been doing nothing for about a week, but I've been working on transitioning to Avalonia, which, now that it seems close to becoming realized, I am replacing with what was previously here. It is unfinished, but hopefully will be fully functional and cross-platform soon!
    Also includes higher-res icon. Still not great looking but w/e.

---
+++ Options.File.Checker/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Options.File.Checker.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
